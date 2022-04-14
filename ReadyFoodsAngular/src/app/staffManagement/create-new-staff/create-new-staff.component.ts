7ac6e63371d777feaf884e468280cf0133f8e1f3
e056 <e0560031@u.nus.edu>
2022-04-15 02:55:08 +0800

    added success message upon creating new staff

--- ReadyFoodsAngular/src/app/staffManagement/create-new-staff/create-new-staff.component.ts
+++ ReadyFoodsAngular/src/app/staffManagement/create-new-staff/create-new-staff.component.ts
@@ -2,6 +2,7 @@ import { AccessRightEnum } from './../../models/access-right-enum';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { NgForm } from '@angular/forms';
+import {Message,MessageService} from 'primeng/api';
 
 import { Staff } from '../../models/staff';
 import { SessionService } from '../../services/session.service';
@@ -11,6 +12,7 @@ import { StaffService } from '../../services/staff.service';
   selector: 'app-create-new-staff',
   templateUrl: './create-new-staff.component.html',
   styleUrls: ['./create-new-staff.component.css'],
+  providers: [MessageService]
 })
 export class CreateNewStaffComponent implements OnInit {
   // public AccessRightEnumLabelMapping = AccessRightEnumLabelMapping;
@@ -29,7 +31,8 @@ export class CreateNewStaffComponent implements OnInit {
     private router: Router,
     private activatedRoute: ActivatedRoute,
     public sessionService: SessionService,
-    private staffService: StaffService
+    private staffService: StaffService,
+    private messageService: MessageService
   ) {
     this.newStaff = new Staff();
     this.resultError = false;
@@ -70,18 +73,19 @@ export class CreateNewStaffComponent implements OnInit {
           let newStaff: Staff = response;
           this.resultSuccess = true;
           this.resultError = false;
-          this.message =
-            'New staff ' + newStaff.firstName + ' created successfully!';
+          // this.message =
+          //   'New staff ' + newStaff.firstName + ' created successfully!';
 
           this.newStaff = new Staff();
           createStaffForm.resetForm();
           createStaffForm.reset();
+          this.messageService.add({severity:'success', summary:'New Staff Created Successfully', detail: "Staff ID: " + newStaff.staffId});
         },
         error: (error) => {
           this.resultError = true;
           this.resultSuccess = false;
-          this.message =
-            'An error has occurred while creating new staff: ' + error;
+
+          this.messageService.add({severity:'error', summary:'Error occured with creating staff', detail: error});
         },
       });
     }

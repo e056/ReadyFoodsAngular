214670191fd0fd061e8710e5c9c2ccae0c2e73e1
Cassia <e0564059@u.nus.edu>
2022-04-17 12:33:31 +0800

    update ingredients

---
+++ ReadyFoodsAngular/src/app/models/update-ingredients-req.spec.ts
@@ -0,0 +1,7 @@
+import { UpdateIngredientsReq } from './update-ingredients-req';
+
+describe('UpdateIngredientsReq', () => {
+  it('should create an instance', () => {
+    expect(new UpdateIngredientsReq()).toBeTruthy();
+  });
+});
